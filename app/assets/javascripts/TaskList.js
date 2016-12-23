32dad57dd9e7aeafa29ec20eaa7b2725808a6c16
Daisuke Suzuki <d_suzuki@tc5.so-net.ne.jp>
2016-12-23 23:26:45 +0900

    Chap4Final

---
+++ app/assets/javascripts/TaskList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Task from "./Task";
+
+export default class TaskList extends React.Component {
+  render() {
+    var tasks = this.props.data.map((task) => {
+      return (
+        <Task key={task.id} id={task.id} content={task.content} status={task.status} onTaskUpdate={this.props.onTaskUpdate} />
+      );
+    });
+    return (
+      <tbody>
+        {tasks}
+      </tbody>
+    );
+  }
+}
